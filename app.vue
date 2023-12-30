bc04d4ccb0e40aab8e2c3cd81073f4bc183603cc
s s <sakasatosh@gmail.com>
2023-12-30 17:10:40 +0900

    feat: defineModel example

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <ModelExampleComponent :count="5"/>
   </div>
 </template>
---
+++ components/ModelExampleComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const model = defineModel<number>('count', { required: true })
+
+const update = () =>  model.value++
+</script>
+
+<template>
+  <div>parent bound v-model is: {{ model }}</div>
+  <button @click="update">update</button>
+</template>
